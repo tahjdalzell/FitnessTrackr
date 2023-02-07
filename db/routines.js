e10d73ffbd6d03077fd37e62ba68b8eb0652c878
TahjD <tahjjobs@gmail.com>
2023-02-06 17:54:54 -0800

    getAllRoutines Working

--- db/routines.js
+++ db/routines.js
@@ -18,8 +18,17 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
   }
 }
 
-async function getRoutineById(id) {}
-
+async function getRoutineById(id) {
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(`SELECT * FROM routines WHERE id = $1;`[id]);
+    return routine;
+  } catch (error) {
+    console.error("Error getting Routine by Id");
+    throw error;
+  }
+}
 async function getRoutinesWithoutActivities() {
   try {
     const { rows: routines } = await client.query(
@@ -38,7 +47,55 @@ async function getRoutinesWithoutActivities() {
   }
 }
 
-async function getAllRoutines() {}
+async function getAllRoutines() {
+  try {
+    const { rows: routines } = await client.query(`
+      SELECT 
+        routines.id, 
+        routines."creatorId", 
+        routines."isPublic", 
+        routines.name, 
+        routines.goal, 
+        users.username AS "creatorName" 
+      FROM 
+        routines 
+      LEFT JOIN 
+        users 
+      ON 
+        routines."creatorId" = users.id
+    `);
+
+    const { rows: routineActivities } = await client.query(`
+      SELECT 
+        routine_activities.id AS "routineActivityId", 
+        routine_activities."routineId", 
+        routine_activities."activityId"
+        AS id, 
+        routine_activities.duration, 
+        routine_activities.count, 
+        activities.name, 
+        activities.description
+      FROM 
+        routine_activities 
+      LEFT JOIN 
+        activities 
+      ON 
+        routine_activities."activityId" = activities.id
+    `);
+
+    routines.forEach((routine) => {
+      routine.activities = routineActivities.filter(
+        (routineActivity) => routineActivity.routineId === routine.id
+      );
+    });
+    // console.log("db", routines);
+
+    return routines;
+  } catch (error) {
+    console.error("Error getting routines!");
+    throw error;
+  }
+}
 
 async function getAllPublicRoutines() {}
 
